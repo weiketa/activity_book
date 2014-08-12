01e7691316fb05fe808fb005416baab40f85177b
weiketa <lyvoctor@gmail.com>
2014-08-12 22:08:33 +0800

    finish user login and login_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def current_user
 
+    if session[:user_id]
+      @current_user ||=User.find(session[:user_id])
+    end
+  end
+
+  helper_method :current_user
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  include BCrypt
   def welcome
   end
 
@@ -9,13 +10,29 @@ class UsersController < ApplicationController
   def login
   end
 
+  def login_out
+    session.delete(:user_id)
+    redirect_to :root
+  end
+
   def create
     @user=User.new(params[:user])
     if @user.save
-      flash[:notice]="你好，#{@user.login}"
+      session[:user_id]=@user.id
       redirect_to :root
     else
       render :signup
+    end
+  end
+
+  def create_login_session
+    user=User.find_by_login(params[:login])
+    if user && user.authenticate(params[:password])
+      session[:user_id]=user.id
+      redirect_to :root
+    else
+      flash[:notice]= '用户名不存在或密码错误'
+      render :login
     end
   end
 
